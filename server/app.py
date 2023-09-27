5c9aea095761b5f3c8ea8c955934edb803c8ca4c
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-09-28 01:38:39 +0300

    passed all the tests

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify, session
+from flask import Flask, make_response, jsonify, session,request
 from flask_migrate import Migrate
 
 from models import db, Article, User
@@ -20,15 +20,62 @@ def clear_session():
     session['page_views'] = 0
     return {'message': '200: Successfully cleared session data.'}, 200
 
-@app.route('/articles')
-def index_articles():
+# @app.route('/articles')
+# def index_articles():
+#     session['page_views'] = session.get('page_views',0) 
+#     # return make_response(jsonify(session), 200)
+#     # response = make_response(jsonify({
+#     #     'session':{
+#     #         'session_value': session['page_views'],
+#     #         'session_accessed': session.accessed,
+#     #     },
+#     #      'cookies': [{cookie: request.cookies[cookie]}
+#     #         for cookie in request.cookies],
+#     # }),200)
+#     # response.set_cookie('mouse', 'Cookie')
+#     # return response
+#     articles = Article.query.all()
+#     articles_list=[]
+#     for article in articles:
+#        article_dict ={
+#             'id':article.id,
+#             'author':article.author,
+#             'title':article.title,
+#             'content':article.content,
+#             'preview':article.preview,
+#             'minutes_to_read':article.minutes_to_read,
+#             'date':article.date,
+#         }
+#        articles_list.append(article_dict)
 
-    pass
+#     return make_response(articles_list,200)
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    session['page_views'] = session.get('page_views',0) 
+    session['page_views'] +=1
+    article =  Article.query.filter_by(id=id).first()
+    if session['page_views'] <=3:
+        # article_dict = article.to_dict()
+        article_dict ={
+          
+            'author':article.author,
+            'title':article.title,
+            'content':article.content,
+            'preview':article.preview,
+            'minutes_to_read':article.minutes_to_read,
+            'date':article.date,
+        }
+        return(jsonify(article_dict),200)
+    
 
-    pass
+    else:
+        r = {'message': 'Maximum pageview limit reached'}
+        return jsonify(r),401
+  
+
+  
+    
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555,debug=True)
